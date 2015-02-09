1c12f22546f97f7ad1e05cc17bda9405388be369
Noppanit Charassinvichai <Noppanit.c@gmail.com>
2015-02-09 08:34:19 -0500

    Initial Revision

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from moviepy.editor import *
+
+UPLOAD_FOLDER = '/tmp'
+
+app = Flask(__name__)
+app.debug = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    file = request.files['filedata']
+    file.save('/tmp/movie.gif')
+
+    video = VideoFileClip("/tmp/movie.gif")
+
+    result = CompositeVideoClip([video]) # Overlay text on video
+    result.write_videofile("/tmp/movie.mp4") # Many options...
+
+    return 'Upload successful'
+
+@app.route("/")
+def tryme():
+    return render_template('hello.html')
+
+if __name__ == "__main__":
+    app.run()
