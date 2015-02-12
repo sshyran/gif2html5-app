893304d940e6480fc7ed8e9fc7f4b2ccd3780e32
Noppanit Charassinvichai <Noppanit.c@gmail.com>
2015-02-12 11:02:21 -0500

    Import Key

--- src/s3_manager.py
+++ src/s3_manager.py
@@ -1,5 +1,6 @@
 import os
 from boto.s3.connection import S3Connection
+from boto.s3.key import Key
 
 class S3Manager:
     def __init__(self, aws_access, aws_secret):
@@ -7,7 +8,7 @@ class S3Manager:
 
     def upload(self, filename, filepath):
         fusion_mp4 = self.conn.get_bucket('fusion-gif2html5-mp4')
-        
+
         k = Key(b)
         k.key = filename
         k.set_contents_from_filename(filepath)
