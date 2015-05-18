8f65a0bb41de89078adfbed23c3dfb4928d4bd1e
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-05-18 17:53:58 -0400

    Added folder to S3 instead of putting everything in root folder

--- src/s3_manager.py
+++ src/s3_manager.py
@@ -7,16 +7,17 @@ class S3Manager:
         aws_access = config.get('AWS_ACCESS_KEY_ID')
         aws_secret = config.get('AWS_SECRET_ACCESS_KEY')
         self.bucket = config.get('BUCKET')
+        self.folder = config.get('FOLDER')
         
         self.conn = S3Connection(aws_access, aws_secret)
         self.fusion_mp4 = self.conn.get_bucket(self.bucket)
 
     def upload(self, filename, filepath):
         k = Key(self.fusion_mp4)
-        k.key = filename
+        k.key = "%s/%s" % (self.folder, filename)
         k.set_contents_from_filename(filepath, policy='public-read')
 
-        return "https://s3.amazonaws.com/%s/%s" % (self.bucket, filename)
+        return "https://s3.amazonaws.com/%s/%s/%s" % (self.bucket, self.folder, filename)
 
     def delete(self, filename):
         k = Key(self.fusion_mp4)
