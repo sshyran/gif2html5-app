0a2e72add8df7db1476f6ac0cb515b557d84b291
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-07-27 15:49:40 -0400

    Pass in exception

--- gif2html5/exceptions/bad_content_type.py
+++ gif2html5/exceptions/bad_content_type.py
@@ -1,6 +1,4 @@
 ''' Custom exception because the url is not a gif so it cannot be converted '''
 
-class BadContentType(Exception):
-
-    def __init__(self, message):
-        self.message = message
+class BadContentType(Exception): 
+    pass
