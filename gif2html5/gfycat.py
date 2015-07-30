7ca173033e65c9607b8bfed5aa70ed97d0d76a7b
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-07-30 14:19:42 -0400

    Added debug to the response from gfycat

--- gif2html5/gfycat.py
+++ gif2html5/gfycat.py
@@ -1,10 +1,18 @@
 import requests
+import logging
+import sys
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
 def convert_gif(gif_url):
     url = 'http://upload.gfycat.com/transcode?fetchUrl=%s' % gif_url
     response = requests.get(url)
     data = response.json()
+
+    logging.debug('Response from {url} is {json}'.format(url=gif_url,
+                                                         json=data))
 
     if 'error' in data:
         return None
